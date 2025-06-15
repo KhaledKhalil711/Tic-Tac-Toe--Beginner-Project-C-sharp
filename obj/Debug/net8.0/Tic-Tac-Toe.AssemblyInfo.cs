accc0d2805ff3d41b2fdd9fc3e03bb87c9b79ef4
KhaledKhalil711 <khaled.khalil@bath.edu>
2025-06-15 11:42:53 +0200

    Made some changes

--- obj/Debug/net8.0/Tic-Tac-Toe.AssemblyInfo.cs
+++ obj/Debug/net8.0/Tic-Tac-Toe.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tic-Tac-Toe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40bc52656f437f4dc8dea2d1f4969b2e0b610c39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tic-Tac-Toe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tic-Tac-Toe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
